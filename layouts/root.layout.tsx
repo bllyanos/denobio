9d183865b5bbfd2ab659f0ddf6002a1585c2554c
Billy Editiano <bllyanos@gmail.com>
2025-08-02 11:45:14 +0700

    feat: add favicon

--- layouts/root.layout.tsx
+++ layouts/root.layout.tsx
@@ -11,6 +11,7 @@ export const RootLayout: FC = (props) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${description}" />
         <title>billy's directory</title>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="stylesheet" href="/style.css" />
         <!-- For Android Chrome -->
         <meta name="theme-color" content="#000" />
